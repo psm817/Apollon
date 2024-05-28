727fd187d0e68e62eca102c8b0ea6c67ca3de282
code-student <dlagorhs99@naver.com>
2024-05-28 22:37:13 +0900

    Maegon/1 (#12)
    * header.html 완성(일부 êí특정 기능 ì구현 시 일부 수정 요함) 및 footer.html 생성
    
    * layout.html(메인 페이지)에 header.html적용되도록 수ì 정, HomeController 수정

--- src/main/java/com/example/Apollon/domain/home/controller/HomeController.java
+++ src/main/java/com/example/Apollon/domain/home/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String test() {
-        return "3조 화이팅!!!!!!!!슈루루루룽1번째 슈루루루룽2번째ㅋㅋㅋ";
+        return "/layout";
     }
 }
