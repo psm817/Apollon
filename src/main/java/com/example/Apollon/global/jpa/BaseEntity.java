e8c25c28da6a057d1ddd9b918c672cd89708f766
psm817 <5004jse@gmail.com>
2024-05-29 18:40:02 +0900

    충돌방지4

--- src/main/java/com/example/Apollon/global/jpa/BaseEntity.java
+++ src/main/java/com/example/Apollon/global/jpa/BaseEntity.java
@@ -18,6 +18,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @ToString
 public class BaseEntity {
+    // 전체 DB 테이블(엔티티)에 포함된 id, createDate, modifyDate의 어노테이션을 관리하는 곳이라고 생각하면 좋음
+    // 각자 엔티티 만들 때, id, createDate, modifyDate는 따로 적용할 필요가 없음
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
