9dcc129ef965778edffcb8d785ac46ebc1c4ad0e
psm817 <150203036+psm817@users.noreply.github.com>
2024-05-29 16:19:30 +0900

    Psm817 (#13)
    * studio 기본 세팅 및 ãinitData, jpa 패키지 생성ê
    
    * layout.html 구조변경, header.html수정, studio_detail.html 테스트 성공
    
    * layout.html 구조변경, header.html수정, studio_detail.html 테스트 성공
    
    * studio_detail.html 작업 시작
    
    * BaseEntity에 있는 컬럼을 사용하기 위해 App에 EnableJpa 어노테이션 추가
    
    * layout.html 주석 추가

---
+++ src/main/java/com/example/Apollon/global/jpa/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.example.Apollon.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass       // 이벤트 발생했을 때 특정 동작 수행
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BaseEntity {
+    // 전체 DB 테이블(엔티티)에 포함된 id, createDate, modifyDate의 어노테이션을 관리하는 곳이라고 생각하면 좋음
+    // 각자 엔티티 만들 때, id, createDate, modifyDate는 따로 적용할 필요가 없음
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+}
