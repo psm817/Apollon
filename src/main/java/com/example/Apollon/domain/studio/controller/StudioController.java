dd3bc58a1b1aa5de02617a1c4feef25b10b92d41
psm817 <5004jse@gmail.com>
2024-05-30 23:47:21 +0900

    컨트롤러 수정 시작

--- src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
+++ src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
@@ -1,5 +1,7 @@
 package com.example.Apollon.domain.studio.controller;
 
+import com.example.Apollon.domain.member.service.MemberService;
+import com.example.Apollon.domain.studio.service.StudioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/studio")
 @RequiredArgsConstructor
 public class StudioController {
+    private final StudioService studioService;
+    private final MemberService memberService;
+
     @GetMapping("/")
     public String detail() {
 
