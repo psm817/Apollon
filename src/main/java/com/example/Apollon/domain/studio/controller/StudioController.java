83f56af3b572f4caeb644ff3fec16661fa406524
psm817 <5004jse@gmail.com>
2024-06-01 01:18:44 +0900

    13

--- src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
+++ src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
@@ -1,12 +1,18 @@
 package com.example.Apollon.domain.studio.controller;
 
+import com.example.Apollon.domain.member.entity.Member;
 import com.example.Apollon.domain.member.service.MemberService;
+import com.example.Apollon.domain.studio.entity.Studio;
 import com.example.Apollon.domain.studio.service.StudioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/studio")
@@ -15,6 +21,7 @@ public class StudioController {
     private final StudioService studioService;
     private final MemberService memberService;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/")
     public String detail() {
 
