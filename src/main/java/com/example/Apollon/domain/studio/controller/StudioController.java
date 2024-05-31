8802673e7a4875cecaabc458a8768f388fb27e03
psm817 <150203036+psm817@users.noreply.github.com>
2024-05-31 14:12:25 +0900

    Psm817 (#18)
    * 충돌방지1
    
    * 충돌방지3
    
    * 충돌방지4
    
    * 충돌방지5
    
    * 충돌방지1
    
    * 충돌방지3
    
    * 충돌방지4
    
    * 충돌방지5
    
    * studio_detail.html 진행중
    
    * studio_detail 작업 중
    
    * studio_detail.html 작업 중
    
    * script 추가
    
    * studio_detail.html 초안 완료
    
    * 충돌방지1
    
    * 충돌방지3
    
    * 충돌방지4
    
    * 충돌수정
    
    * studio_detail.html 진행중
    
    * studio_detail 작업 중
    
    * studio_detail.html 작업 중
    
    * script 추가
    
    * studio_detail.html 초안 완료
    
    * 충돌방지
    
    * 충돌방지4
    
    * member_id 엔티티 추가
    
    * 컨트롤러 수정 시작
    
    * css border 제거

--- src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
+++ src/main/java/com/example/Apollon/domain/studio/controller/StudioController.java
@@ -1,5 +1,7 @@
 package com.example.Apollon.domain.studio.controller;
 
+import com.example.Apollon.domain.member.service.MemberService;
+import com.example.Apollon.domain.studio.service.StudioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/studio")
 @RequiredArgsConstructor
 public class StudioController {
+    private final StudioService studioService;
+    private final MemberService memberService;
+
     @GetMapping("/")
-    public String mine() {
+    public String detail() {
 
         return "studio_detail";
     }
