ce80ef8fc60c76e92dc818c57a60f9f07f3e5372
ilmong06 <ilmongcoding@gmail.com>
2024-06-02 03:04:31 +0900

    구글 로그인

--- src/main/java/com/example/Apollon/global/security/CustomOAuth2UserService.java
+++ src/main/java/com/example/Apollon/global/security/CustomOAuth2UserService.java
@@ -1,9 +1,10 @@
 package com.example.Apollon.global.security;
-
 //
 //import com.example.Apollon.domain.member.entity.Member;
 //import com.example.Apollon.domain.member.service.MemberService;
 //import lombok.RequiredArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -22,48 +23,74 @@ package com.example.Apollon.global.security;
 //@Transactional(readOnly = true)
 //@RequiredArgsConstructor
 //public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+//    private static final Logger log = LoggerFactory.getLogger(CustomOAuth2UserService.class);
 //    private final MemberService memberService;
 //
-//    // 카카오톡 로그인이 성공할 때 마다 이 함수가 실행된다.
 //    @Override
 //    @Transactional
 //    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 //        OAuth2User oAuth2User = super.loadUser(userRequest);
-//
-//        String oauthId = oAuth2User.getName();
-//        Map<String, Object> attributes = oAuth2User.getAttributes();
-//
-//        Map attributesProperties = (Map) attributes.get("properties");
-//        String nickname = (String) attributesProperties.get("nickname");
-//
 //        String providerTypeCode = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
 //
-//        String username = providerTypeCode + "__%s".formatted(oauthId);
+//        if (providerTypeCode.equals("KAKAO")) {
+//            log.info("카카오 로그인");
+//            return processKakaoLogin(oAuth2User);
+//        } else if (providerTypeCode.equals("NAVER")) {
+//            log.info("네이버 로그인");
+//            return processNaverLogin(oAuth2User);
+//        }
 //
-//        Member member = memberService.whenSocialLogin(providerTypeCode, username, nickname);
 //
-//        List<GrantedAuthority> authorityList = new ArrayList<>();
+//        // 다른 소셜 미디어 타입 처리 가능
 //
-//        return new CustomOAuth2User(member.getUsername(), member.getPassword(), authorityList);
-//    }
-//}
-//
-//class CustomOAuth2User extends User implements OAuth2User {
-//
-//    public CustomOAuth2User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-//        super(username, password, authorities);
-//    }
-//
-//    @Override
-//    public Map<String, Object> getAttributes() {
 //        return null;
 //    }
 //
-//    @Override
-//    public String getName() {
-//        return getUsername();
+//    private OAuth2User processKakaoLogin(OAuth2User oAuth2User) {
+//        String oauthId = oAuth2User.getName();
+//        Map<String, Object> attributes = oAuth2User.getAttributes();
+//        Map<String, Object> attributesProperties = (Map<String, Object>) attributes.get("properties");
+//        String nickname = (String) attributesProperties.get("nickname");
+//        String providerTypeCode = "KAKAO";
+//        String username = providerTypeCode + "__%s".formatted(oauthId);
+//        Member member = memberService.whenSocialLogin(providerTypeCode, username, nickname);
+//        List<GrantedAuthority> authorityList = new ArrayList<>();
+//        return new CustomOAuth2User(member.getUsername(), member.getPassword(), authorityList);
+//    }
+//
+//    private OAuth2User processNaverLogin(OAuth2User oAuth2User) {
+//        // 네이버 로그인 처리 추가
+//        // 네이버 사용자 정보 가져오기
+//        String nickname = oAuth2User.getAttribute("nickname");
+//        String providerTypeCode = "NAVER";
+//        String oauthId = oAuth2User.getAttribute("id");
+//        String username = providerTypeCode + "__%s".formatted(oauthId);
+//        Member member = memberService.whenSocialLogin(providerTypeCode, username, nickname);
+//        List<GrantedAuthority> authorityList = new ArrayList<>();
+//        return new CustomOAuth2User(member.getUsername(), member.getPassword(), authorityList);
+//    }
+//
+//
+//    class CustomOAuth2User extends User implements OAuth2User {
+//
+//        public CustomOAuth2User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+//            super(username, password, authorities);
+//        }
+//
+//        @Override
+//        public Map<String, Object> getAttributes() {
+//            return null;
+//        }
+//
+//        @Override
+//        public String getName() {
+//            return getUsername();
+//        }
 //    }
 //}
+
+
+
 
 
 import com.example.Apollon.domain.member.entity.Member;
@@ -104,6 +131,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         } else if (providerTypeCode.equals("NAVER")) {
             log.info("네이버 로그인");
             return processNaverLogin(oAuth2User);
+        } else if (providerTypeCode.equals("GOOGLE")) { // 수정된 부분
+            log.info("구글 로그인"); // 수정된 부분
+            return processGoogleLogin(oAuth2User); // 수정된 부분
         }
 
         // 다른 소셜 미디어 타입 처리 가능
@@ -129,6 +159,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String nickname = oAuth2User.getAttribute("nickname");
         String providerTypeCode = "NAVER";
         String oauthId = oAuth2User.getAttribute("id");
+        String username = providerTypeCode + "__%s".formatted(oauthId);
+        Member member = memberService.whenSocialLogin(providerTypeCode, username, nickname);
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        return new CustomOAuth2User(member.getUsername(), member.getPassword(), authorityList);
+    }
+
+    private OAuth2User processGoogleLogin(OAuth2User oAuth2User) {
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String oauthId = (String) attributes.get("sub");
+        String nickname = (String) attributes.get("name");
+        String email = (String) attributes.get("email");
+        String providerTypeCode = "GOOGLE";
         String username = providerTypeCode + "__%s".formatted(oauthId);
         Member member = memberService.whenSocialLogin(providerTypeCode, username, nickname);
         List<GrantedAuthority> authorityList = new ArrayList<>();
