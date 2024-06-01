2fc2e242f6a46f925bb818e18b674ebf2b5f4b0d
maegon <dlagorhs99@naver.com>
2024-06-01 17:09:19 +0900

    커서 추가

--- src/main/java/com/example/Apollon/domain/music/controller/MusicController.java
+++ src/main/java/com/example/Apollon/domain/music/controller/MusicController.java
@@ -1,15 +1,43 @@
 package com.example.Apollon.domain.music.controller;
 
+import com.example.Apollon.domain.music.entity.Music;
+import com.example.Apollon.domain.music.service.MusicService;
+import com.example.Apollon.domain.post.entity.Post;
+import com.example.Apollon.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/chart")
+@RequiredArgsConstructor
 public class MusicController {
-    @GetMapping("/music")
-    public String musicPlayer(Model model) {
-        String[] playlist = {"곡1", "곡2", "곡3", "곡4", "곡5"};
-        model.addAttribute("playlist", playlist);
-        return "redirect:/";
+    private final MusicService musicService;
+
+    // TOP100
+    @GetMapping("/TOP100")
+    public String top100List(Model model) {
+        List<Music> top100List = this.musicService.getTOP100List();
+
+        model.addAttribute("TOP100", top100List);
+
+        return "chart/TOP100";
+    }
+
+
+    // 곡 정보
+    @GetMapping("/musicDetail/{id}")
+    public String musicDetail(Model model, @PathVariable("id") Long id) {
+
+        Music m = musicService.getMusic(id);
+
+        model.addAttribute("music", m);
+
+        return "music/musicDetail";
     }
 }
--- src/main/java/com/example/Apollon/domain/music/entity/Music.java
+++ src/main/java/com/example/Apollon/domain/music/entity/Music.java
@@ -1,14 +1,32 @@
 package com.example.Apollon.domain.music.entity;
 
+import com.example.Apollon.domain.member.entity.Member;
 import com.example.Apollon.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Music extends BaseEntity {
-
     private String musicTitle;
-    private String user;
+    private String musicContent;
+    private String[] genres;
+    private String uploadStudio;
 
+    @Getter
+    private Long musicPlayCount;
+
+    //나중에 좋아요 넣을 것
 }
