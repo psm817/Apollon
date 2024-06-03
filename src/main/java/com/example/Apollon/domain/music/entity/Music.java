affc103d07f76a612c31d9953e5eeb46111d2da8
code-student <dlagorhs99@naver.com>
2024-06-03 15:23:38 +0900

    커서 변경, 브라우저 홈페이지 로고 추가 (#29)
    * 블ë메인 ë블럭 작성중
    
    * 블ë메인 ë블럭 작성중
    
    * ì카ì카오 로그인 ãì최신화 전
    
    * 브라우저 ã 로고 추가
    
    * 헤더 fixed 적용
    
    * 헤더 픽스 취소
    
    * 커서 추가
    
    * 메인페이지 스크롤 추가
    
    * 커서 모양 바ã꿈, 색상 랜덤 색 지정, 플레이바 아이콘 기능 디자인 몇개 수정
    
    * 메인페이지 블럭ì구분선 수정
    
    * 좋ì음악 좋아요 기능 틀만 구성
    
    * TOP100 경로 지정 재설정, 슬슬 TOP100 시작
    
    * 커서 반변경 주기
    
    * 최신 메인화 오류 수정
    
    * 뭐지?
    
    * 블ë메인 ë블럭 작성중
    
    * ì카ì카오 로그인 ãì최신화 전
    
    * 브라우저 ã 로고 추가
    
    * 헤더 fixed 적용
    
    * 5
    
    * 커서 추가
    
    * 메인페이지 스크롤 추가
    
    * 커서 모양 바ã꿈, 색상 랜덤 색 지정, 플레이바 아이콘 기능 디자인 몇개 수정
    
    * 메인페이지 블럭ì구분선 수정
    
    * 좋ì음악 좋아요 기능 틀만 구성
    
    * TOP100 경로 지정 재설정, 슬슬 TOP100 시작
    
    * 커서 반변경 주기

--- src/main/java/com/example/Apollon/domain/music/entity/Music.java
+++ src/main/java/com/example/Apollon/domain/music/entity/Music.java
@@ -1,14 +1,40 @@
 package com.example.Apollon.domain.music.entity;
 
+import com.example.Apollon.domain.member.entity.Member;
 import com.example.Apollon.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Music extends BaseEntity {
-
     private String musicTitle;
-    private String user;
+    private String musicContent;
+    private String[] genres;
+    private String uploadStudio;
 
+    @Getter
+    private Long musicPlayCount;
+
+    @ManyToMany
+    @JoinTable(
+            name = "music_member_likes",
+            joinColumns = @JoinColumn(name = "music_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id")
+    )
+    private Set<Member> likedByMembers = new LinkedHashSet<>();
 }
