1ab15e893bff8bc7b7dead82f1bbf90c12bc2001
tron0318 <tron0318@naver.com>
2024-06-10 18:08:43 +0900

    í 커뮤니티 페이징구현

--- src/main/java/com/example/Apollon/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/Apollon/domain/post/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.example.Apollon.domain.post.repository;
 
 import com.example.Apollon.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAll(Pageable pageable);
 }
