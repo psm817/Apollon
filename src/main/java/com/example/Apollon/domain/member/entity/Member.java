481b59798126ab549df041690134408c27e312dc
maegon <dlagorhs99@naver.com>
2024-06-11 15:09:56 +0900

    r r

--- src/main/java/com/example/Apollon/domain/member/entity/Member.java
+++ src/main/java/com/example/Apollon/domain/member/entity/Member.java
@@ -1,14 +1,14 @@
 package com.example.Apollon.domain.member.entity;
 
 
-import com.example.Apollon.domain.music.entity.Music;
+import com.example.Apollon.domain.playlist.entity.Playlist;
 import com.example.Apollon.domain.studio.entity.Studio;
 import com.example.Apollon.global.jpa.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.LazyCollection;
@@ -28,7 +28,6 @@ public class Member extends BaseEntity {
     @Column(unique = false)
     private String nickname;
     private String email;
-
     @Column(unique = false)
     private String image; // 수정된 부분: 이미지 파일 이름을 저장하는 필드
 
@@ -37,12 +36,4 @@ public class Member extends BaseEntity {
     @LazyCollection(LazyCollectionOption.EXTRA)
     private Studio studio;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // 이미지 필드의 setter 메서드 추가
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
