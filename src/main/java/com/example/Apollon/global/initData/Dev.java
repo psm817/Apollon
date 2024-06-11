811a771f4c62a96d9a409fbf179589f45f6a8126
maegon <dlagorhs99@naver.com>
2024-06-11 14:59:15 +0900

    음악 등록 경로 수정(아마 이게 최종?)

--- src/main/java/com/example/Apollon/global/initData/Dev.java
+++ src/main/java/com/example/Apollon/global/initData/Dev.java
@@ -37,14 +37,15 @@ public class Dev {
 
     @Bean
     public ApplicationRunner init(MemberService memberService, StudioService studioService, CommentService commentService) {
+
         return args -> {
             Member m1 = memberService.signup("admin", "admin", "admin", "admin@test.com","");
             Member m2 = memberService.signup("user1", "user1", "user1", "user1@test.com","");
             Member m3 = memberService.signup("user2", "user2", "user2", "user2@test.com","");
 
-            Studio s1 = studioService.createOrUpdate(m1, 55,1);
-            Studio s2 = studioService.createOrUpdate(m2, 12,1);
-            Studio s3 = studioService.createOrUpdate(m3, 5,1);
+            Studio s1 = studioService.createOrUpdate(m1, 55, 1);
+            Studio s2 = studioService.createOrUpdate(m2, 12, 1);
+            Studio s3 = studioService.createOrUpdate(m3, 5, 1);
 
             commentService.create(m2, s3, "테스트입니다.1", "테스트입니다.1");
             commentService.create(m2, s3, "테스트입니다.2", "테스트입니다.2");
@@ -54,89 +55,6 @@ public class Dev {
             commentService.create(m3, s2, "테스트입니다.2", "테스트입니다.2");
             commentService.create(m3, s2, "테스트입니다.3", "테스트입니다.3");
 
-
-            // 음악 재생을 위한 테스트 데이터 1
-            // 음악 파일 상대 경로 (지정된 경로 사용)
-            String thumbnailDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadImgs\\옥.png";
-            // 음악 파일 상대 경로 (지정된 경로 사용)
-            String musicDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadMusics\\Zedd & Alessia Cara - Stay.mp3";
-
-            String thumbnailRelPath = UUID.randomUUID() + ".jpg";
-            String musicFileRelPath = UUID.randomUUID() + ".mp3";
-
-            File thumbnailFile = new File(thumbnailDirPath + "/" + thumbnailRelPath);
-            File musicFile = new File(musicDirPath + "/" + musicFileRelPath);
-
-            String title = "Zedd & Alessia Cara - Stay"; // 음악 제목
-            String content = "Zedd & Alessia Cara의 Stay입니다."; // 음악 내용
-            String member = m1.getUsername(); // 스튜디오 멤버
-                       String[] genres = {"Electronica", "etc"}; // 장르
-
-            Music music = Music.builder()
-                    .musicTitle(title)
-                    .musicContent(content)
-                    .uploadStudio(member)
-                    .thumbnailImg("/uploadImgs/" + thumbnailFile)
-                    .musicMp3("/uploadMusics/" + musicFile)
-                    .genres(genres)
-                    .musicPlayCount(Long.valueOf("324"))
-                    .build();
-            // 테스트 데이터 저장
-            musicRepository.save(music);
-
-            // 음악 재생을 위한 테스트 데이터 2
-            thumbnailDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadImgs\\좀벗.jpg";
-            musicDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadMusics\\Zedd - Clarity.mp3";
-
-            thumbnailRelPath = UUID.randomUUID() + ".jpg";
-            musicFileRelPath = UUID.randomUUID() + ".mp3";
-
-            thumbnailFile = new File(thumbnailDirPath + "/" + thumbnailRelPath);
-            musicFile = new File(musicDirPath + "/" + musicFileRelPath);
-
-            title = "Zedd - Clarity"; // 음악 제목
-            content = "Zedd의 Clarity입니다."; // 음악 내용
-            member = m3.getUsername(); // 스튜디오 멤버
-            genres = new String[]{"Electronica", "etc"}; // 장르
-
-            music = Music.builder()
-                    .musicTitle(title)
-                    .musicContent(content)
-                    .uploadStudio(member)
-                    .thumbnailImg("/uploadImgs/" + thumbnailFile)
-                    .musicMp3("/uploadMusics/" + musicFile)
-                    .genres(genres)
-                    .musicPlayCount(Long.valueOf("650"))
-                    .build();
-            // 테스트 데이터 저장
-            musicRepository.save(music);
-
-            // 음악 재생을 위한 테스트 데이터 3
-            thumbnailDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadImgs\\와보.png";
-            musicDirPath = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadMusics\\Diamond Eyes - Stay.mp3";
-
-            thumbnailRelPath = UUID.randomUUID() + ".jpg";
-            musicFileRelPath = UUID.randomUUID() + ".mp3";
-
-            thumbnailFile = new File(thumbnailDirPath + "/" + thumbnailRelPath);
-            musicFile = new File(musicDirPath + "/" + musicFileRelPath);
-
-            title = "Diamond Eyes - Stay"; // 음악 제목
-            content = "Diamond Eyes의 Stay입니다."; // 음악 내용
-            member = m2.getUsername(); // 스튜디오 멤버
-            genres = new String[]{"Electronica", "etc"}; // 장르
-
-            music = Music.builder()
-                    .musicTitle(title)
-                    .musicContent(content)
-                    .uploadStudio(member)
-                    .thumbnailImg("/uploadImgs/" + thumbnailFile)
-                    .musicMp3("/uploadMusics/" + musicFile)
-                    .genres(genres)
-                    .musicPlayCount(Long.valueOf("134"))
-                    .build();
-            // 테스트 데이터 저장
-            musicRepository.save(music);
         };
     }
 }
