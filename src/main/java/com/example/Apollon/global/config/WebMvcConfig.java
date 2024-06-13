fada78cddfab4525868ecd4b964006caa8222eae
psm817 <5004jse@gmail.com>
2024-06-13 16:49:30 +0900

    사진 불러오기 경로 변경

--- src/main/java/com/example/Apollon/global/config/WebMvcConfig.java
+++ src/main/java/com/example/Apollon/global/config/WebMvcConfig.java
@@ -10,6 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/uploads/**")
-                .addResourceLocations("file:///C:/Users/user/IdeaProjects/Apollon/src/main/resources/static/images/uploads/");
+                .addResourceLocations("file:///C:/work/Apollon/src/main/resources/static/images/uploads/");
     }
 }
