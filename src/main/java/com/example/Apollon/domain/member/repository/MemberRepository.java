8f22cccd60c016cad199846d9d919117a79abf6c
ilmong06 <ilmongcoding@gmail.com>
2024-06-15 00:27:52 +0900

    비밀번호 찾기페이지에서 아이디도 추가

--- src/main/java/com/example/Apollon/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/Apollon/domain/member/repository/MemberRepository.java
@@ -12,6 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
     Optional<Member> findByEmail(String email);
     boolean existsByUsername(String username); // 중복된 아이디 확인 메서드 추가
-
+    Optional<Member> findByUsernameAndEmail(String username, String email); // username과 email이 동시에 일치하는 회원 조회
     Optional<Member> findById(Long memberId);
 }
