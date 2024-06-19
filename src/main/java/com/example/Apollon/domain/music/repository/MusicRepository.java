aee5f49e47751aed92ea85935a150c0e905c1ab5
psm817 <5004jse@gmail.com>
2024-06-19 14:42:00 +0900

    충돌1

--- src/main/java/com/example/Apollon/domain/music/repository/MusicRepository.java
+++ src/main/java/com/example/Apollon/domain/music/repository/MusicRepository.java
@@ -4,6 +4,7 @@ import com.example.Apollon.domain.music.entity.Music;
 import com.example.Apollon.domain.studio.entity.Studio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
