65ba14c41ee75ed986d04982bec8dfa17343f6b0
A <161570008+ilmong06@users.noreply.github.com>
2024-06-24 16:26:28 +0900

    Ahjeong (#93)
    * 병합충돌
    
    * 병합충돌
    
    * 수정
    
    * 타임라인 기능 추가
    
    * 볼륨기능
    
    * 이전곡,다음곡,푸터높이,볼륨
    
    * 반복기능 추가
    
    * 세션스í스토리지 를 ì 이용한 곡 유지 기능
    
    * readme 수정
    
    * readme 수정
    
    * readme 수정
    
    * readme수정
    
    * readme수정
    
    * reademe수정
    
    * 반복 버튼 색상 변경
    
    * 다음곡 í재생 함수 수정
    
    * 세션스토리지 삭제
    
    * readme수정
    
    * 반복기능 총정리
    
    * 반복기능,새로고침 시 곡 유지 기능,리드미수정
    
    * 샘플데이터 삭제
    
    * 샘플데이터 삭제,리드미 수정

--- src/main/java/com/example/Apollon/domain/music/service/MusicService.java
+++ src/main/java/com/example/Apollon/domain/music/service/MusicService.java
@@ -55,7 +55,7 @@ public class MusicService {
 
     public String storeImg(MultipartFile profilePicture) {
 
-        String uploadDir = "C:\\work\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadImgs";
+        String uploadDir = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadImgs";
 
         Path uploadPath = Paths.get(uploadDir);
         if (!Files.exists(uploadPath)) {
@@ -78,7 +78,7 @@ public class MusicService {
 
     public String storeMp3(MultipartFile profilePicture) {
 
-        String uploadDir = "C:\\work\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadMusics";
+        String uploadDir = "C:\\Users\\user\\IdeaProjects\\Apollon\\src\\main\\resources\\static\\uploadFile\\uploadMusics";
 
         Path uploadPath = Paths.get(uploadDir);
         if (!Files.exists(uploadPath)) {
