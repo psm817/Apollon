968fdf70b539c0623735ade305ce3118b92607cd
psm817 <150203036+psm817@users.noreply.github.com>
2024-06-25 23:47:39 +0900

    전체 디자인 수정, 장르별차트 서클 추가, 플레이리스트 이퀄 추가, 좋아요, 담기 추가 (#97)

--- src/main/java/com/example/Apollon/global/initData/DataFileUtils.java
+++ src/main/java/com/example/Apollon/global/initData/DataFileUtils.java
@@ -1,17 +1,30 @@
 package com.example.Apollon.global.initData;
 
+import org.springframework.stereotype.Component;
+
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 // 폴더에 쌓이는 테스트 데이터 정리를 위한 샘플 파일(와보2.png, stay.mp3)을 제외한 데이터 삭제
+@Component
 public class DataFileUtils {
 
-    public static void deleteFilesExcept(String directoryPath, String exceptionFileName) {
+    // 삭제하지 않을 파일들의 이름 목록
+    private static final List<String> EXCEPT_FILE_NAMES = Arrays.asList(
+            "clarity.png", "rose.png", "stay.png", "closer.png",
+            "Zedd - Clarity.mp3", "The Chainsmokers - Roses.mp3",
+            "Zedd & Alessia Cara - Stay.mp3", "The Chainsmokers Feat Halsey - Closer (JayKode Remix).mp3"
+    );
+
+    public static void deleteFilesExcept(String directoryPath, String... exceptionFileNames) {
         File directory = new File(directoryPath);
         File[] files = directory.listFiles();
 
         if (files != null) {
             for (File file : files) {
-                if (!file.getName().equals(exceptionFileName)) {
+                // 파일 이름이 삭제하지 않을 파일들의 목록에 포함되어 있지 않으면 삭제
+                if (!EXCEPT_FILE_NAMES.contains(file.getName())) {
                     file.delete();
                 }
             }
